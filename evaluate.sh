8b358f9a7e26c72491e9c676f6d5a1023b45b2d7
LudekCizinsky <ludekcizinsky11@gmail.com>
2025-10-29 08:30:33 +0100

    cleanup

--- evaluate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Conda init and activate
-source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
-conda activate thesis
-
-# Evaluate
-# python training/evaluate.py internal_run_id=cool-sea-51_f2rqogjf split=train
-python training/evaluate.py internal_run_id=ancient-wildflower-55_3ne7d465 split=val
