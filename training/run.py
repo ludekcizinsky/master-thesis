a0cb4eb6be95a8d7dffb73b4515f100e3f53c5cc
LudekCizinsky <ludekcizinsky11@gmail.com>
2025-11-18 11:04:16 +0100

    saving the latest fg render progress to the checkpoint dir

--- training/run.py
+++ training/run.py
@@ -79,6 +79,7 @@ class Trainer:
             cfg.tids,
         )
         self.checkpoint_dir = self.ckpt_manager.root
+        self.ckpt_fg_render_dir = self.checkpoint_dir / "fg_render"
         self.baseline_vis_dir = Path(self.cfg.preprocess_dir) / "baseline_smpl_vis"
 
         if not cfg.resume:
@@ -850,6 +851,11 @@ class Trainer:
         if len(selected_tids) > 0:
             save_qual_dir = self.experiment_dir / "visualizations" / "fg_render" / "all" / f"epoch_{epoch:04d}"
             save_qual_rgb_dir = save_qual_dir / "rgb"
+            save_checkpoint_rgb_dir = self.ckpt_fg_render_dir / "all" / "rgb"
+            # if exists, clean the checkpoint dir to save new eval renders
+            if save_checkpoint_rgb_dir.exists():
+                shutil.rmtree(save_checkpoint_rgb_dir)
+            os.makedirs(save_checkpoint_rgb_dir, exist_ok=True)
             save_qual_mask_dir = save_qual_dir / "gt_mask"
             os.makedirs(save_qual_rgb_dir, exist_ok=True)
             os.makedirs(save_qual_mask_dir, exist_ok=True)
@@ -891,6 +897,8 @@ class Trainer:
                 img_np = (colors[0].cpu().numpy() * 255).astype("uint8")
                 img_pil = Image.fromarray(img_np)
                 img_pil.save(save_qual_rgb_dir / f"{fid:04d}.png")
+                # also save to checkpoint dir
+                img_pil.save(save_checkpoint_rgb_dir / f"{fid:04d}.png")
 
                 # save the mask image
                 mask_np = (gt_masks_joined[0].cpu().numpy() * 255).astype("uint8")
