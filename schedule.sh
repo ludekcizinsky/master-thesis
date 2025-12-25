a9bef63e0f333df59c83fadeb39da8742dac776c
LudekCizinsky <ludekcizinsky11@gmail.com>
2025-12-25 20:25:00 +0100

    wip

--- schedule.sh
+++ schedule.sh
@@ -4,5 +4,5 @@ source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
 module load gcc ffmpeg
 conda activate thesis
 
-exp_name="est_masks_gt_smplx_h3_use_smplx_based_masks_for_nv"
+exp_name="v2_est_masks_gt_smplx_h2_smplx_mesh_based"
 python /home/cizinsky/master-thesis/schedule.py --exp_name $exp_name --job_name_prefix $exp_name
