8f59e7443e65f77052a5bb0d034e186de310f9e2
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-06 20:23:05 +0100

    wip

--- run_debug.sh
+++ run_debug.sh
@@ -19,12 +19,12 @@ cd /home/cizinsky/master-thesis
 # python playground/different_sources_body_models_check.py --source_a_scene_dir $src_a_dir --source_a_name $src_a_name --source_b_scene_dir $src_b_dir --source_b_name $src_b_name --src_cam_id 4 --body_model_kind $body_model_kind
 
 
-# SMPLX to SMPL conversion debug
-scene_dir=/scratch/izar/cizinsky/thesis/preprocessing/hi4d_pair19_piggyback
-# - conversion
-bash /home/cizinsky/master-thesis/submodules/smplx/tools/run_conversion.sh $scene_dir smplx smpl
-# - check conversion
-python playground/body_model_conversion_check.py --scene-dir $scene_dir --model-folder /home/cizinsky/body_models --src-cam-id 4 --image-width 940 --image-height 1280
+# # SMPLX to SMPL conversion debug
+# scene_dir=/scratch/izar/cizinsky/thesis/preprocessing/hi4d_pair19_piggyback
+# # - conversion
+# bash /home/cizinsky/master-thesis/submodules/smplx/tools/run_conversion.sh $scene_dir smplx smpl
+# # - check conversion
+# python playground/body_model_conversion_check.py --scene-dir $scene_dir --model-folder /home/cizinsky/body_models --src-cam-id 4 --image-width 940 --image-height 1280
 
 # # SMPL to SMPLX conversion debug
 # scene_dir=/scratch/izar/cizinsky/ait_datasets/full/hi4d/pair16/pair16/jump16
@@ -32,3 +32,13 @@ python playground/body_model_conversion_check.py --scene-dir $scene_dir --model-
 # bash /home/cizinsky/master-thesis/submodules/smplx/tools/run_conversion.sh $scene_dir smpl smplx
 # # - check conversion
 # python playground/body_model_conversion_check.py --scene-dir $scene_dir --model-folder /home/cizinsky/body_models --src-cam-id 4 --image-width 940 --image-height 1280
+
+
+# Reformat and align debug
+bash preprocess/custom/run_reformat_and_align.sh
+src_a_dir=/scratch/izar/cizinsky/thesis/preprocessing/hi4d_pair16_jump
+src_a_name="estimated"
+src_b_dir=/scratch/izar/cizinsky/ait_datasets/full/hi4d/pair16/pair16/jump16
+src_b_name="ground_truth"
+body_model_kind="smplx"
+python playground/different_sources_body_models_check.py --source_a_scene_dir $src_a_dir --source_a_name $src_a_name --source_b_scene_dir $src_b_dir --source_b_name $src_b_name --src_cam_id 4 --body_model_kind $body_model_kind
