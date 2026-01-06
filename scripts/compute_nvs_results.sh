fe12c99963c5f21a564c80f7e574a6bf9987693f
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-06 09:21:12 +0100

    more results

--- scripts/compute_nvs_results.sh
+++ scripts/compute_nvs_results.sh
@@ -5,7 +5,7 @@ source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
 module load gcc ffmpeg
 conda activate thesis
 
-exp_name="v3_est_masks_gt_smplx_h6_rgb_smplx_band_and_apply_mask_to_renders"
+exp_name="v1_gt_masks_est_smplx_h1_use_h3r_no_smplx_tune_during_training"
 epoch_str="0150"
 
 scene_names=(
