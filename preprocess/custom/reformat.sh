a941b8f1fa4b4c4fca831906197d9c4801587ea6
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-09 19:39:47 +0100

    wip

--- preprocess/custom/reformat.sh
+++ preprocess/custom/reformat.sh
@@ -3,7 +3,7 @@ set -e # exit on error
 
 cd /home/cizinsky/master-thesis
 
-# bash preprocess/custom/helpers/rename_and_copy.sh
-# bash preprocess/custom/helpers/cameras_reformat.sh
-# bash preprocess/custom/helpers/smplx_reformat.sh
+bash preprocess/custom/helpers/rename_and_copy.sh
+bash preprocess/custom/helpers/cameras_reformat.sh
+bash preprocess/custom/helpers/smplx_reformat.sh
 bash preprocess/custom/helpers/smplx_align.sh
