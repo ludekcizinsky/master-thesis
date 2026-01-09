5d450a6832247d2dc6fa4b4fa88bfb44151ad308
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-09 13:11:04 +0100

    wip

--- preprocess/custom/run_reformat.sh
+++ preprocess/custom/run_reformat.sh
@@ -5,4 +5,4 @@ cd /home/cizinsky/master-thesis
 
 bash preprocess/custom/helpers/other_reformat.sh
 bash preprocess/custom/helpers/cameras_reformat.sh
-bash preprocess/custom/helpers/smplx_reformat.sh
+# bash preprocess/custom/helpers/smplx_reformat.sh
