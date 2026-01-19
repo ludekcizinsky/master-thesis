7b365dd885ceb577d22925cb95b43ae43851591e
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-19 20:05:18 +0100

    wip

--- schedule.sh
+++ schedule.sh
@@ -4,5 +4,5 @@ source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
 module load gcc ffmpeg
 conda activate thesis
 
-exp_name="v203_core_ablation_set_a3_use_lhm_with_view_densification_but_no_difix"
+exp_name="v203_core_ablation_set_a4_use_lhm_with_view_densification_and_difix"
 python /home/cizinsky/master-thesis/schedule.py --exp_name $exp_name --job_name_prefix $exp_name
