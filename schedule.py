0925746c258c82652f525bbe3fc9f2de4bdaf370
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-28 22:43:42 +0100

    wip

--- schedule.py
+++ schedule.py
@@ -63,7 +63,7 @@ class Scene:
 class ScheduleConfig:
     job_name_prefix: str = "difix_v9_baseline"
     exp_name: str = "difix_v8_baseline"
-    time: str = "06:00:00"
+    time: str = "10:00:00"
     slurm_script: str = "train.slurm"
     scene_name_includes: str | None = None
     dry_run: bool = False
--- schedule.sh
+++ schedule.sh
@@ -4,3 +4,8 @@ source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
 module load gcc ffmpeg
 conda activate thesis
 
+# Baseline experiment however evaluated on all tasks and scenes
+exp_name="v975_A0b_baseline_combo_all_scenes_eval"
+python /home/cizinsky/master-thesis/schedule.py \
+    --exp_name $exp_name \
+    --job_name_prefix $exp_name
