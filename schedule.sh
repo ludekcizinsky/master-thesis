436e9d0110cc1dd62c076f0c3fe69ecdfa600acb
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-01-29 14:14:53 +0100

    wip

--- schedule.sh
+++ schedule.sh
@@ -4,8 +4,16 @@ source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
 module load gcc ffmpeg
 conda activate thesis
 
-# Baseline experiment however evaluated on all tasks and scenes
+# Baseline using TSDF to get meshes
 exp_name="v975_A0b_baseline_combo_all_scenes_eval"
 python /home/cizinsky/master-thesis/schedule.py \
     --exp_name $exp_name \
-    --job_name_prefix $exp_name
+    --job_name_prefix $exp_name \
+    --overrides gs_to_mesh_method=tsdf
+
+
+exp_name="v984_mc_for_3dgs_to_mesh"
+python /home/cizinsky/master-thesis/schedule.py \
+    --exp_name $exp_name \
+    --job_name_prefix $exp_name \
+    --overrides gs_to_mesh_method=mc
