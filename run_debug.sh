1167394538441548e1c73fa6130b186e8ae15165
LudekCizinsky <ludekcizinsky11@gmail.com>
2026-02-05 15:51:04 +0100

    wip

--- run_debug.sh
+++ run_debug.sh
@@ -10,3 +10,5 @@ conda activate thesis
 # navigate to project directory
 cd /home/cizinsky/master-thesis
 
+estimated_scene_dir=/scratch/izar/cizinsky/thesis/v2_preprocessing/hi4d_pair19_piggyback
+python preprocess/vis/check_scene_in_3d.py --scene-dir $estimated_scene_dir --src-cam-id 4 --frame-idx-range 100 150
